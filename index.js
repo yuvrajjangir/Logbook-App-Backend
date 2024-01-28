0303cb681b2f6f4df0a71fd2583dd05513517f2e
yuvrajjangir <yuvijangir78@gmail.com>
2024-01-28 20:11:44 +0530

    done-intial

--- index.js
+++ index.js
@@ -27,6 +27,20 @@ app.use('/expenses', Expenserouter);
 app.use('/logbook', StatsRouter);
 app.use('/', PdfRouter);
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+  
+    if (req.method === 'OPTIONS') {
+      res.sendStatus(200); // Respond to preflight request
+    } else {
+      next();
+    }
+  });
+
+  
 const PORT = process.env.PORT;
 app.listen(PORT, async (req, res) => {
     try {
