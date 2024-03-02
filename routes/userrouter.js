052b97626fd3f9b6654837a3e689d17247bd7833
yuvrajjangir <yuvijangir78@gmail.com>
2024-03-02 15:45:58 +0530

    done

--- routes/userrouter.js
+++ routes/userrouter.js
@@ -5,6 +5,15 @@ const {UserModel} = require("../model/authmodel");
 const bcrypt = require("bcrypt");
 const cors = require("cors");
 
+// Configure CORS middleware
+const corsOptions = {
+  origin: 'https://tax-tim-log-book-app.vercel.app',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+// Apply CORS middleware to all routes
+UserRouter.use(cors(corsOptions));
+
 UserRouter.post("/signup", (req, res) => {
     console.log(req.body);
     const {name, email, password} = req.body;
@@ -51,7 +60,5 @@ UserRouter.post("/login", async (req, res) => {
         return res.status(500).send(error.message);
     }
 });
-
-UserRouter.use(cors());
 
 module.exports = {UserRouter};
